742c953c432c8df673d40fc87048aeb8109f4dbd
Chris Londrie <chris.londrie@galvanize.com>
2023-03-24 11:41:42 -0500

    Nested Loops Example

--- src/main.js
+++ src/main.js
@@ -426,41 +426,101 @@ let data = [
     }
 ]
 
-let loadsOfArrays = [
-    [
-        ["apples", "oranges", "lemons", "limes"],
-        ["potato", "carrots", "beans", "lentils"]
-    ],
-    [
-        ["pepsi", "mountain dew", "sprite"],
-        ["coke", "diet coke", "mr. pibb"]
-    ]
-
-]
-let simplerArray = ["apples", "oranges", "lemons", "limes", "potato", "carrots", "beans", "lentils", "pepsi", "mountain dew", "sprite", "coke", "diet coke", "mr. pibb"]
-console.log(loadsOfArrays[0][1][1]);
-
-// Write a function, that checks if a givenItem is in any of the arrays and returns the index of that item.
-
-function findGivenItem(itemArray, givenItem) {
-    for (var i = 0; i < itemArray.length; i++) {
-        for (var j = 0; j < itemArray[i].length; j++) {
-            for (var k = 0; k < itemArray[i][j].length; k++) {
-                if (itemArray[i][j][k].toLowerCase() == givenItem.toLowerCase()) {
-                    return [i, j, k];
-                }
-            }
-        }
-    }
-}
-
-// function findGivenItem2(itemArray, givenItem) {
-//     for (var i = 0; i < itemArray.length; i++) {
-//         if (itemArray[i].toLowerCase() == givenItem.toLowerCase()) {
-//             return i;
-//         }
+// function refine(num) {
+//     if (num === 5) {
+//         return num
 //     }
+//     num = num + 1;
+//     return refine(num);
 // }
 
-let output = findGivenItem(loadsOfArrays, "cArRotS");
-console.log(`The item is at index: ${output}`);
+// function aFunction() {
+
+//     let output = refine(3)
+//     return output = output + 1;
+// }
+
+
+// let output = aFunction();
+// console.log(output);
+
+// function isItTruthy(param) {
+//     if (param) {
+//       return 'Input is truthy';
+//       } else {
+//       console.log('It is falsy', param);
+//       return 'Input is falsy';
+
+//       }        
+//   }
+//  isItTruthy();
+
+//   if (true) {
+
+//   }
+
+//   if (false) {
+
+//   }
+
+//   function determineIfTrue(param) {
+
+//     if (param != false) { 
+//       console.log('Input is True');
+//       return 'Input is True';  
+
+//     } else { 
+//       console.log('Input is False');
+//       return 'Input is False';
+
+//    }
+//   }
+//   determineIfTrue(1);
+
+// console.log("3" == 3);
+
+var person = { 
+    name: "Jim", 
+    age: 21,
+    cars: ["Ferrari", "Geo Metro"]
+ }
+var age = "cars"
+console.log(person.age)
+console.log(person[age])
+console.log(person["cars"])
+arrayOfObjects = [
+    {name: "Laurie", age: -1, cars: ["Rogue", "Airstream"]},
+    {name: "Beckham"},
+    {name: "Jim"},
+    {name: "Christian"}
+]
+
+console.log(arrayOfObjects[0].cars[1])
+
+var siding = ["aluminum", "vinyl"]
+var windows = ["solid", "paned"]
+
+var house = {}
+
+const enumComponents = {
+    siding: 1,
+    windows: 2
+}
+
+function takeAnArray(inputArray, component) {
+    switch (component) {
+        case enumComponents.siding:
+
+        break;
+        case enumComponents.windows:
+
+        break;
+
+        default:
+            console.log("Invalid component given")
+            return -1;
+    }
+
+}
+
+takeAnArray(siding, enumComponents.siding)
