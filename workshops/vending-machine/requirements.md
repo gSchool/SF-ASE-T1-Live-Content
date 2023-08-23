0b43d5aa3a14545bc354f1127b2b72bd3ad745d8
Richard Key <rich.key@galvanize.com>
2023-08-23 10:04:00 -0700

    New Workshop

---
+++ workshops/vending-machine/requirements.md
@@ -0,0 +1,21 @@
+# The Infinite Vending Machine
+
+Your is to create the logic for the infinite vending machine. 
+
+Your solution should follow these requirements:
+* You should create a function that accepts a location and an amount of money (in cents)
+* The location is a letter + number combination, such as "B2"
+* The number in the location starts at 1, not 0
+* The number in the location represents the placement in the array, you will need to account for the numbers starting at 1
+* Your logic must verify the money provided is enough to purchase the item at the location
+* The function should return an object that contains the item and the change from the purchase
+* If the given money is not enough, the returned item should be null and the change should be all the money given
+* There should be a message printed if the money is not enough
+* There should be a message printed about the item purchased, using the return of the function
+
+Keep in mind there is not just one solution to this project. Your solution might vary from others.
+
+## Bonus Additions
+
+* Add a random chance for the machine to fail at "picking" the item, returning nothing but still taking the money
+* Start the code (outside the function) by printing out a vending machine graphic will all the items in it (with prices)
---
+++ workshops/vending-machine/starter.js
@@ -0,0 +1,42 @@
+const machine = {
+  'a': [
+    {"name": "SnackNova Potato Crisps","price": 83},
+    {"name": "Chippity Doo-Dahs","price": 447},
+    {"name": "Tatertronics","price": 310},
+    {"name": "Krusty Brand Imitation Potato Chips","price": 139}
+  ],
+  'b': [
+    {"name": "WholesomeWhirl Snackin' Crisps","price": 191},
+    {"name": "QuirkyCrunch Cheesy Animal Crackers","price": 201},
+    {"name": "Taternado Potato Twisters","price": 334},
+    {"name": "Crispy Crunchos","price": 442}
+  ],
+  'c': [
+    {"name": "YumYam Crisps","price": 60},
+    {"name": "CraveWave Corn Puffs","price": 275},
+    {"name": "Completely Normal Potato Chips","price": 54},
+    {"name": "CrispMagic Ultra Crispy Chips","price": 290}
+  ],
+  'd': [
+    {"name": "VelvetCrunch Bar","price": 305},
+    {"name": "MarshmallowWhirl Bar","price": 113},
+    {"name": "SBLOUNSKCHED! Bar","price": 449},
+    {"name": "PeanutPraline Perfection","price": 82},
+    {"name": "ChocoBurst Delight","price": 403},
+    {"name": "LemonLush Bar","price": 297}
+  ],
+  'e': [
+    {"name": "Chunky Blast Offs","price": 150},
+    {"name": "Mr. Candy Bar","price": 127},
+    {"name": "Scooby Snack","price": 59},
+    {"name": "Super Mega Ultra Giant Jawbreaker","price": 408},
+    {"name": "Power Crunch Bar","price": 205}, 
+    {"name": "Squishee Bar","price": 174}
+  ]
+};
+
+// Some example locations to use in testing:
+// c1
+// a3
+// e5
+
