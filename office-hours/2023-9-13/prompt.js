bb34a09cae4d403042917df154068d0f96513ea1
Richard Key <rich.key@galvanize.com>
2023-09-13 09:22:46 -0700

    Group prompt for OH

---
+++ office-hours/2023-9-13/prompt.js
@@ -0,0 +1,8 @@
+// define a function that takes a string as a parameter
+// this function should return an array that contains three (or less) letter segments
+// Ex:
+// "1234567890" -> ["123", "456", "789", "0"]
+// "this is a string!" -> ["thi", "s i", "s a", " st", "rin", "g!"]
+
+// Stretch Goal: take a second parameter that is used to determine the length of the segments
+// splitThisString("1234567890", 4) -> ["1234", "5678", "90"]
