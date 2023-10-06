2e47fc403deba54156d2fbac45b10b6915444854
Danny Tunon <danieltunon@gmail.com>
2023-10-06 07:31:10 -1000

    basic solution implemented

--- office-hours/2023-10-6/problem.js
+++ office-hours/2023-10-6/problem.js
@@ -1,11 +1,42 @@
 // write a function that builds an array of objects, based on an input array of numbers
 // it will map each number in the array to an object in the new array
 // this function will have one parameter: an array of numbers
-// it will return an array or objects, with each object having the following properties (keys):
+function buildArrayofObjects(input){
+    let newArray = [];
+
+    for (let i = 0; i < input.length; i ++) {
+        let numObject = {
+            divide: (input[i] / 2),
+            multiply: (input[i] * 2),
+            add: (input[i] + 2),
+            subtract: (input[i] - 2),
+        }
+        newArray.push(numObject);
+    }
+    //console.log(newArray);
+    return newArray;
+}
+const nums1 = [2,10]
+const res1 = buildArrayofObjects(nums1)
+console.log("nums1:",nums1)
+console.log("res1",res1)
+
+
+//ex
+    //newArray = [
+        //create object{
+            //key divide: //math
+            //key multiply: //math
+        //}
+        
+    //]
+// it will return an array of objects, with each object having the following properties (keys):
 // divide - the number divided by two
 // multiply - the number multipled by two
 // add - the number plus 2
 // subtract - the number minus 2
+
+
 // Example:
 // for the input [2, 10]
 // the output should be 
@@ -13,6 +44,8 @@
 //   { divide: 1, multiply: 4, add: 4, subtract: 0 } 
 //   { divide: 5, multiply: 20, add: 12, subtract: 8 }
 // ]; 
+// example map of strings to their lengths:
+// ["hello", "goodbye", "sun"] --> [5, 7, 3]
 
 
 // stretch goal: update the funciton to take a "modifier" parameter to use in the calculations in place of a static 2.
