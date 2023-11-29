ece4948d0491818138ddaac14be58be0c1abed78
Richard Key <rich.key@galvanize.com>
2023-11-29 15:43:11 -0700

    Talkin about functions

---
+++ office-hours/2023-11-29/functions.js
@@ -0,0 +1,27 @@
+function add(x, y) {
+  return x + y;
+}
+
+let sum = add(5, 10);
+let sum2 = add(3, 7);
+
+// I want to add two numbers together
+// what would a invocation look like for a function
+// that adds two numbers?
+// let sum = addTwoNumbers(5, 10);
+
+// Take some code that already exists and
+// apply your example to it:
+// let x = 5;
+// let y = 10;
+// let sum = x + y;
+// sumNumbers(x, y) OR sumNumbers(5, 10) OR sumNumbers(10, 5);
+// let sum = sumNumbers(5, 10);
+
+// let x2 = 3;
+// let x3 = 7;
+// let sum2 = x2 + y2;
+// let sum2 = sumNumbers(3, 7);
+
+
+
