b43ab7a94ce6395d13e2a1a580235fdb537afdaf
Ian Culleton <ian.culleton.92@gmail.com>
2024-02-21 12:58:49 -0500

    Create prompt.js

---
+++ office-hours/2024-2-21/prompt.js
@@ -0,0 +1,10 @@
+// Write a function that takes a string as an argument and returns 
+// a number representing the longest contiguous string of identical 
+// characters.  For example, if the input string was "looong" then
+// the function would return 3, since there are 3 contiguous "o" 
+// characters in the input string.  A few more examples:
+
+// input "eventhough" returns 1, because the two e's are not contiguous.
+// input "jello" returns 2, since the 2 l's are next to each other.
+// input "wwwhinne" returns 3.
+
